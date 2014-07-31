d4642916c8926727a25a4ae226fe6fe6c56c52ed
mattblickem <explosivose@gmail.com>
2014-07-31 12:08:17 +0100

    prototyped terror spider procedural animation

---
+++ Assets/BrainStorm/Terror/Scripts/SpiderLegAnimator.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderLegAnimator : MonoBehaviour {
+
+	public enum Leg {
+		FrontLeft, FrontRight, BackLeft, BackRight
+	}
+	public Leg thisLegIs = Leg.FrontLeft;
+	
+	public Transform spiderBody;
+	public Transform target;
+	public Transform elbow;
+	public float legMoveSpeed;
+	public float animationSpeed;
+	public float targetSearchRadius;
+	public float legLength;
+	
+	
+	private Vector3 previousTarget;
+	private Vector3 updateTarget;
+	private Vector3 updateElbow;
+	
+	private float lastMoveTime;
+	
+	void Start () {
+		StartCoroutine( Animate() );
+	}
+	
+	void Update () {
+		float t = Time.time - lastMoveTime;
+		target.position =  Vector3.Lerp(target.position, updateTarget, legMoveSpeed * Time.deltaTime);
+
+		elbow.position = Vector3.Lerp(elbow.position, updateElbow, 1f * Time.deltaTime);
+	}
+	
+	IEnumerator Animate() {
+		if (animationSpeed == 0f) animationSpeed = 0.001f;
+		yield return new WaitForSeconds(Random.value * 1f/animationSpeed);
+		while(this.enabled) {
+			Vector3? t = LegTarget();
+			if (t.HasValue)  {
+				previousTarget = updateTarget;
+				updateTarget = t.Value; 
+				lastMoveTime = Time.time;
+			}
+			
+			updateElbow = ElbowTarget(transform.position, updateTarget);
+			
+			if (animationSpeed == 0f) animationSpeed = 0.001f;
+			yield return new WaitForSeconds(1f/animationSpeed);
+		}
+	}
+	
+	Vector3? LegTarget() {
+		Vector3 reachDirection;
+		switch (thisLegIs) {
+		default:
+		case Leg.FrontLeft:
+			reachDirection = spiderBody.forward - spiderBody.right;
+			break;
+		case Leg.FrontRight:
+			reachDirection = spiderBody.forward + spiderBody.right;
+			break;
+		case Leg.BackLeft:
+			reachDirection = -spiderBody.forward - spiderBody.right;
+			break;
+		case Leg.BackRight:
+			reachDirection = -spiderBody.forward + spiderBody.right;
+			break;
+		}
+		
+		Ray ray = new Ray(transform.position, reachDirection);
+		RaycastHit hit;
+		if (Physics.SphereCast(ray, targetSearchRadius, out hit, legLength)) {
+			return hit.point;
+		}
+		else {
+			return null;
+		}
+	}
+	
+	Vector3 ElbowTarget(Vector3 body, Vector3 target) {
+		return new Vector3( 
+			(body.x + target.x) / 2,
+			10f + (body.y + target.y) / 2,
+			(body.z + target.z) / 2
+		);
+	}
+	
+}
