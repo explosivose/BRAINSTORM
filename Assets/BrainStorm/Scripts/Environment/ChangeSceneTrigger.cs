f86d790808adcb1e1cdc635b9b9b03a5f5ce30fe
mattblickem <explosivose@gmail.com>
2014-08-28 23:38:26 +0100

    combined armory and lobby scenes

--- Assets/BrainStorm/Scripts/Environment/ChangeSceneTrigger.cs
+++ Assets/BrainStorm/Scripts/Environment/ChangeSceneTrigger.cs
@@ -1,22 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[System.Serializable]
-public class Scene {
-	public enum Tag {
-		Lobby, Armory, Alone, Rage, Terror
-	}
-	
-	public string name {
-		get {
-			return tag.ToString();
-		}
-	}
-	public Tag tag;
-	public Transform scenePrefab;
-	
-}
-
 public class ChangeSceneTrigger : MonoBehaviour {
 
 	public Scene.Tag changeTo = Scene.Tag.Lobby;
---
+++ Assets/BrainStorm/Scripts/Environment/Scene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Scene {
+	public enum Tag {
+		Lobby, Alone, Rage, Terror
+	}
+	
+	public string name {
+		get {
+			return tag.ToString();
+		}
+	}
+	public Tag tag;
+	public Transform scenePrefab;
+	
+}
